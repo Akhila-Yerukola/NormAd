d1b83d377912bcc6268ace71cb94d159addc6d2f
Abhinav Rao <39939017+AetherPrior@users.noreply.github.com>
2026-06-23 16:37:55 +0530

    Add run script

---
+++ src/v2_fix_neutral/run.sh
@@ -0,0 +1,3 @@
+python regenerate_explanations.py --save_only_sample
+python regenerate_rot_value.py --save_only_sample
+python regenerate_explanations.py --input_path ../../output/v2_fix_neutral/normad_etiquette_final_data_rot_regenerated.csv --output_path ../../output/v2_fix_neutral/158_normad_etiquette_final_data_explanations_regenerated.csv --filter entail_bad --sample_size -1 --save_only_sample
